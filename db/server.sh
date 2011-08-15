0a4e1a883121ee74cb1572773525e92d71098954
unknown <Jura@.(none)>
2011-08-15 17:18:12 +0100

    Added database files

--- db/server.sh
+++ db/server.sh
@@ -1,1 +1,1 @@
-java -classpath ../target/MemoryCards_1.0/WEB-INF/lib/hsqldb.jar org.hsqldb.Server -database test
+java -cp ../target/MemoryCards_1.0/WEB-INF/lib/hsqldb.jar org.hsqldb.Server -database test
---
+++ db/start db server.bat
@@ -0,0 +1,1 @@
+java -cp ..\target\MemoryCards_v1.0\WEB-INF\lib\hsqldb-1.8.0.10.jar org.hsqldb.Server -database test
