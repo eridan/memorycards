93d42fd5f5b5f01b4d1f4550730abc7909ac32b5
Vova Barankin <jura_ir@mail.ru>
2011-08-16 12:27:44 +0100

    Updating db server start/stop files

--- db/server.sh
+++ db/server.sh
@@ -1,1 +1,1 @@
-java -cp ../target/MemoryCards_1.0/WEB-INF/lib/hsqldb.jar org.hsqldb.Server -database test
+java -cp ../target/MemoryCards_1.0/WEB-INF/lib/hsqldb-1.8.0.10.jar org.hsqldb.Server -database test
